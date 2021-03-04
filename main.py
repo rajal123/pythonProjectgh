d6f0b1d51b2ea08b783eb64a0de9e73011ef65dd
unknown <rajalshrestha100@gmail.com>
2021-03-04 09:31:58 +0545

    apple calculation

---
+++ main.py
@@ -0,0 +1,11 @@
+# N student take K apples and distribute them among each students eventually.
+# the remaining parts remain in basket.
+# how many apples should each student get?
+# how many apples will remain in the basket?the programs read number N and K
+
+N=int(input("enter the number of students in class: "))
+K=int(input("enter the nuber of apples: "))
+apples_get=(K//N)
+remaining_apples =(K%N)
+print(f"each student get {apples_get} ")
+print(f"the remaining apples are {remaining_apples} ")
