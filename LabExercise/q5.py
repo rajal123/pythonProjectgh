0b99cad4374d464852efd52225deceb0e1d5c6af
unknown <rajalshrestha100@gmail.com>
2021-03-17 09:30:18 +0545

    total desk

--- LabExercise/q5.py
+++ LabExercise/q5.py
@@ -15,7 +15,7 @@ no_student_class3 = int(input("Enter the number of students in third class : "))
 desk_class1 = (no_student_class1//2)
 print(f"the required number of desk for first class is {desk_class1}")
 desk_class2 = (no_student_class2//2)
-print(f"the required number of desk for second class is {des_class2} ")
+print(f"the required number of desk for second class is {desk_class2} ")
 desk_class3 = (no_student_class3//2)
 print(f"the required number of desk for third class is {desk_class3}")
 
